4d6e68a2ed9b6ae960f789d9cda6de20e1d91f23
Phạm Hà Anh <89311003+D-RubyIV@users.noreply.github.com>
2023-03-08 01:03:28 +0700

    Create javaFunction.java

---
+++ Java/src/java_Function/javaFunction.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package java_Function;
+
+/**
+ *
+ * @author phamh
+ */
+public class javaFunction {
+
+	public static void main(String[] args) {
+		int a = 2, b = 3;
+		sum(a,b);
+		System.out.println("Multiplication " + a +"*"+ b +"="+ multipliction(a, b));
+
+	}
+
+	public static void sum(int a, int b) {
+		int sum = a + b;
+		System.out.println("Sum:" + sum);
+	}
+	public static int multipliction(int a, int b) {
+		int mul = a * b;
+		return mul;
+	}
+
+}
