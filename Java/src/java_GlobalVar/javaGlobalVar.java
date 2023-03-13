fb0b7f0c83c7016f9d5e91c1814d5a24da8dc875
Phạm Hà Anh <89311003+D-RubyIV@users.noreply.github.com>
2023-03-13 23:05:49 +0700

    Create javaGlobalVar.java

---
+++ Java/src/java_GlobalVar/javaGlobalVar.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package java_GlobalVar;
+
+/**
+ *
+ * @author phamh
+ */
+public class javaGlobalVar {
+
+	static int a = 5;
+
+	public static void main(String[] args) {
+		System.out.println("So A:" + a);
+		show();
+
+	}
+
+	public static void show() {
+		System.out.println("So A:" + a);
+	}
+
+}
