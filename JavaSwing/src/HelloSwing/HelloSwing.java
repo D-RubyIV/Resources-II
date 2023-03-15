6fd783e731289f3c8ba505d8e66a15f47266531f
Phạm Hà Anh <89311003+D-RubyIV@users.noreply.github.com>
2023-03-15 14:23:46 +0700

    Create HelloSwing.java

---
+++ JavaSwing/src/HelloSwing/HelloSwing.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package HelloSwing;
+
+import javax.swing.JFrame;
+
+/**
+ *
+ * @author phamh
+ */
+public class HelloSwing {
+	public HelloSwing(){
+		JFrame frame = new JFrame("Hello");
+		frame.setVisible(true);
+		frame.setSize(400, 400);
+}
+	public static void main(String[] args) {
+		HelloSwing hs = new HelloSwing();
+		
+	}
+	
+}
+
